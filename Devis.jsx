231990bbbc7b358e24b6be47af3a51e5ad1752a4
thekingpiecesautos-oss <thekingpiecesautos@gmail.com>
2026-03-16 00:06:22 +0100

    Update Devis.jsx

--- Devis.jsx
+++ Devis.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useMemo, useRef, useState } from "react";
-import logo from "./logo.png";
+import logo from "./king-pieces.jpeg";
 import { fetchStore, saveStore } from "./api";
 
 function Devis() {
